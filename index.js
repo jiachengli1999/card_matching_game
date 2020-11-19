ab0ed2c78f97a16543787fba4d9f7e0be0f86342
jiachengli1999 <35944405+jiachengli1999@users.noreply.github.com>
2020-11-19 11:54:06 -0500

    added card background, fixed clicking and matching cards feature

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ let grid_container = $('.grid-container')
 let images = ['astrologian', 'bard', 'black_mage', 'blue_mage', 'dancer', 
                 'dark_knight', 'Dragoon', 'gun', 'gunbreaker', 'monk', 'ninja', 
                 'paladin']
+let background = 'none'
 let clickedCard = null 
 let cardid = null 
+let canClick = false 
 let matched = 0
 
 // shuffle referenced from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
-    // dup values in array
+    // dup values in array to get two of the same card in array
     let res = array.slice()
     for (let i=0; i < images.length; i++){
         res.push(images[i])
@@ -31,15 +33,31 @@ function shuffle(array) {
 }
 
 function main(){
+    // reset
+    clickedCard = null 
+    cardid = null 
+    canClick = false 
+    matched = 0
+
     // shuffle
     let cards = shuffle(images)
+    //set back of card
+    background = "url('./among_us_assets/among_us.PNG')"
 
     // Make grid
     grid_container.empty();
     for (let i=0; i < 24; i++){
         // style=\"background-image: url('./among_us_assets/"+cards[i]+".PNG');\"
         grid_container.append($("<div class='card "+cards[i]+"' id='card-"+i+"'></div>"))
-        // $('#card-'+i).css('background-image', "url('./among_us_assets/"+cards[i]+".PNG')")
+        $('#card-'+i).css('background-image', "url('./among_us_assets/"+cards[i]+".PNG')")
+    }
+
+    // hide cards after some time
+    for (let i=0; i < cards.length; i++){
+        setTimeout(function(){
+            $('#card-'+i).css('background-image', background)
+            canClick = true
+        }, 2000)
     }
 
     // add click listener for each card
@@ -48,33 +66,44 @@ function main(){
 
 function cardListener(cards){
     grid_container.find('.card').bind('click', function(){
-        if (!($(this).hasClass('matched'))){
+        // card can be clicked if not already matched, not the same card, and currently not comparing two cards
+        if (!($(this).hasClass('matched')) && canClick && $(this).attr('id') != cardid){
+            // get image name
             let card_name = $(this).attr('class').split(/\s+/)[1]
+            // assign image to clicked card
             $(this).css('background-image', "url('./among_us_assets/"+card_name+".PNG')")
             $(this).addClass('open')
+
+            // assign first card if it's the first card 
             if (clickedCard === null){
                 clickedCard = $(this).attr('class')
                 cardid = $(this).attr('id')
-                console.log('clickedcard:', clickedCard, cardid)
+                // console.log('clickedcard:', clickedCard, cardid)
             }
+            // else compare the two clicked cards
             else{
-                if ($(this).attr('class') === clickedCard && $(this).attr('id') != cardid){
+                canClick = false
+                // if match keep on board 
+                if ($(this).attr('class') === clickedCard){
                     grid_container.find('.open').addClass('matched');
                     grid_container.find('.matched').removeClass('open');
                     matched++;
-                    console.log('matched')
+                    // console.log('matched')
+                    canClick = true
                 }
+                // if not a match, then flip over 
                 else{
-                    console.log('no match')
                     setTimeout(function () {
-                        grid_container.find('.open').css('background-image', 'none')
+                        grid_container.find('.open').css('background-image', background)
                         grid_container.find('.open').removeClass('open'); 
-                    },600)
+                        canClick = true
+                    },1000)
                 }
                 clickedCard = null
             }
             
-            if (matched === cards.length){
+            // win 
+            if (matched === cards.length/2){
                 console.log('win')
             }
         }
